a99c94d557baed0bd34109c610c271318e74e847
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-01-19 17:17:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true 
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :read_first, presence: true
-  validates :read_family, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+  validates :read_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+  validates :read_family, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
   validates :birth, presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+    message: 'は英数字の混合である必要があります'
+  },on: :create
+
 end
