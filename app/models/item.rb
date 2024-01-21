124fe44e2b620f785c122158d6dcff7d5e7b87a8
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-01-21 20:39:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,15 @@ class Item < ApplicationRecord
   belongs_to :user 
   has_one_attached :image
 
+  validates :image, presence: true 
+  validates :name, presence: true 
+  validates :category_id, presence: true 
+  validates :condition_id, presence: true 
+  validates :postage_type_id, presence: true 
+  validates :prefecture_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :price , presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
