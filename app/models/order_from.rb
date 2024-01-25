f369f7be947a7d02d56885d2baad01021769cd48
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-01-25 14:02:41 +0900

    バリデーションと保存の機能追加

--- app/models/order_from.rb
+++ app/models/order_from.rb
@@ -1,3 +1,18 @@
-class OrderForm
+class OrderFrom
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :street, :building_name, :phone_number, :order_id, :item_id, :user_id
+
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+  validates :building_name, presence: true
+  validates :phone_number, presence: true
+  validates :order_id, presence: true
+  validates :item_id, presence: true
+  validates :user_id, presence: true
+
+  def save
+    @oder =oder.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building_name: building_name, phone_number: phone_number, order_id: order_id, item_id: item_id, user_id: user_id )
+  end
 end
