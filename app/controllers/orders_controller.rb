d204b04774d8f74dbf957b12b56dbc0cf4232215
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-01-26 11:45:43 +0900

    データベースに保存機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,22 @@
 class OrdersController < ApplicationController
 
   def index
-    @order =Order.new
+    @item =Item.find(params[:item_id])
+    @order_from =OrderFrom.new
   end
 
   def create
-    @order=Order.create(order_params)
-    @order.save
+    @item = Item.find(params[:item_id])
+    @order_from = OrderFrom.new(order_params)
+    if @order_from.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
   def order_params
-    params.require(:order_from).permit(:postal_code, :prefecture_id, :city, :street, :building_name, :phone_number,:order_id,:item_id,:user_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_from).permit(:postal_code, :prefecture_id, :city, :street, :building_name, :phone_number).merge(user_id: current_user.id,item_id: @item.id)
   end
 end
-
--- app/models/order_from.rb
+++ app/models/order_from.rb
@@ -6,13 +6,13 @@ class OrderFrom
   validates :prefecture_id, presence: true
   validates :city, presence: true
   validates :street, presence: true
-  validates :building_name, presence: true
   validates :phone_number, presence: true
   validates :order_id, presence: true
   validates :item_id, presence: true
   validates :user_id, presence: true
-
+  
   def save
-    @oder =oder.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building_name: building_name, phone_number: phone_number, order_id: order_id, item_id: item_id, user_id: user_id )
+    order=Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building_name: building_name, phone_number: phone_number, order_id: order.id )
   end
 end
